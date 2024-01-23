32a73a2eb76c577121c2739fc415d8d4ef5c90af
towkzdev <satjaphon.tonkla@gmail.com>
2024-01-24 04:09:51 +0700

    fuction ep58-60

---
+++ EP60createFunction.py
@@ -0,0 +1,20 @@
+#การสร้าง Funcetion / เรียก function
+
+# def ชื่อฟังก์ชัน() :
+#       statement(กลุ่มคำสั่ง)
+
+def say_Hi():
+    print("Hello Function")
+
+def say_Thailand():
+    print("สวัสดี")
+
+def add():
+    x=3+1
+    print(x)
+
+#โปรแกรมหลัก
+    
+say_Thailand()
+say_Hi()
+add()
