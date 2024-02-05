564e3972e79d19a790a28470ba592a119267bbd3
SATJAPHON PANYAFU <satjaphon.tonkla@gmail.com>
2024-02-06 02:06:55 +0700

    ep82

---
+++ EP82Exception.py
@@ -0,0 +1,17 @@
+#Eception (ข้อผิดพลาด)
+
+'''
+sum = 10+"ก"
+print(sum)
+'''
+score = "555"
+number_1= int(input("Enter number :"))
+number_2=int(input("Enter number :"))
+
+result = number_1+number_2
+print(score)
+print(result)
+
+
+
+
