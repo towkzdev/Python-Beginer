a2c845556d4c42e1e19d8acb98983a11edb4a131
towkzdev <satjaphon.ton@gmail.com>
2024-06-12 10:06:36 +0700

    leanning-for-py

---
+++ For_Students/leanning_py.py
@@ -0,0 +1,73 @@
+'''
+#การสร้าง comment(code ในบรรทัดนั้นจะไม่ run) บน coding ภาษา python 
+#1. ใช้สัญลักษณ์ (#)
+    #comment เป็นบรรทัด
+    
+#2.ใช้สัญลักษณ์ "(bubble code)
+"""(เปิด)
+ตัวอย่างการทำงาน
+a = 10
+b = 5
+c = a+b
+print(C)
+(ปิด)"""
+
+#3.ใช้สัญลักษณ์ '(single code)
+(เปิด)
+ตัวอย่างการทำงาน
+(ปิด)
+
+print("test_COMMENT")
+z=20
+x=5
+y=x-z
+print(y)
+'''
+#function print() หรือ การแแสดงผลออกทางหน้าจอ (output)
+print("ตัวดำเนินการ") #ใน () คือตัวดำเนินการ
+print("ข้อความ")
+print("Hello word")
+print(10)
+print(-5)
+
+#การแสดงผลรูปแบบข้อความจะอยู่ในเครื่องหมาย "" หรือ '' (String)
+print("-5")
+print('10')
+print('สวัสดี')
+print("Hello")
+
+a = "10"
+b = "5"
+c = a+b #การต่อ string
+print("Output :",c) #c = 105
+
+#number => ตัวเลข integer(จำนวนเต็ม) , float(ทศนิยม)
+print(1) #int
+print(-1) #int
+print(0) #int
+print(3.99) #float
+print(1+9) #int
+
+#boolean(ค่าความจริง) => true(เป็นจริง,1) , false(เป็นเท็จ,0) แสดงค่าในรูปแบบค่าความจริง
+print(True)
+print(False)
+
+#data type & variable ชนิดเเละรูปแบบของข้อมูล
+'''
+int(ตัวเลข-จำนวนเต็ม)
+float(ตัวเลข-ทศนิยม)
+boolean(ค่าความจริง)
+str(string-ข้อความ)
+'''
+
+# กฏการตั้งชื่อ
+'''
+1.ตัวเลข ตัวอัษร เครื่องหมายพิเศษ ( - , _ , . )
+2.ห้ามขึ้นต้นด้วยตัวเลขและสัญลักษณ์ ยกเว้น...
+3.ห้ามซ้ำกับ Keyword ("print","input","output","True","int")
+4.case sensitive
+'''
+
+_a = 10
+a_1 = 10
+
